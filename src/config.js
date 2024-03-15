5f4443fb0ad9df31be7c180a9e77f4b5be4ec94e
TrinitroToluen0 <javiermenco404@gmail.com>
2024-03-14 23:19:02 -0500

    Enhance config and environment variables

--- src/config.js
+++ src/config.js
@@ -3,14 +3,14 @@ dotenv.config();
 const { ActivityType } = require("discord.js");
 
 module.exports = {
-    DEV_MODE: false,
-    DEV_GUILDS_ID: ["903180945765564438", "747876685302595647"],
-    DEV_USER_ID: "528408424728363029",
-    MONGODB_URI: process.env.MONGODB_URI,
-    BOT_TOKEN: process.env.BOT_TOKEN,
-    CLIENT_ID: "1195233538115637308",
+    DEV_MODE: process.env.NODE_ENV !== "production",
     ACTIVITY_NAME: "The 100",
     ACTIVITY_TYPE: ActivityType.Watching,
+    MONGODB_URI: process.env.MONGODB_URI,
+    CLIENT_ID: process.env.CLIENT_ID,
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    DEV_USER_ID: "528408424728363029",
+    DEV_GUILDS_ID: ["903180945765564438"],
     INTERCHAT_MOD_TEAM: ["528408424728363029", "1009711006429216788", "665066080909393921", "1015430766295011378"],
     SUPPORT_SERVER_INVITE: "https://discord.gg/yJbFXmxQKC",
 };
