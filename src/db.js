44cc494c94277eb8e418df8350423569409f7ae1
TrinitroToluen0 <javiermenco404@gmail.com>
2024-03-14 19:27:50 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 
 async function connectDB(uri) {
+    if (!uri) {
+        throw new Error("MongoDB URI must be provided.");
+    }
     try {
-        if (!uri) {
-            throw new Error("MongoDB URI must be provided.");
-        }
         const db = await mongoose.connect(uri);
         logger.info(`Connected to database: ${db.connection.db.databaseName}`);
     } catch (error) {
