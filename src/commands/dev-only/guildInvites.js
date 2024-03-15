23ade3241cb79ef1ad525404c01d81a7b450eff2
TrinitroToluen0 <javiermenco404@gmail.com>
2024-03-15 17:38:22 -0500

    New command to get the invites of a guild

---
+++ src/commands/dev-only/guildInvites.js
@@ -0,0 +1,40 @@
+const { Interaction, SlashCommandBuilder, PermissionsBitField, EmbedBuilder, Colors } = require("discord.js");
+const { invitation } = require("../../utils/emojis.json");
+
+module.exports = {
+    category: "",
+    cooldown: 5,
+    devOnly: true,
+    botPermissions: [PermissionsBitField.Flags.UseExternalEmojis],
+    data: new SlashCommandBuilder()
+        .setName("guild-invites")
+        .setDescription("Get the invites of a guild")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
+        .addStringOption((option) => option.setName("id").setDescription("The ID of the guild")),
+
+    /**
+     * @param {Interaction} interaction
+     */
+    async execute(interaction) {
+        const guildId = interaction.options.getString("id") || interaction.guild.id;
+        const guild = await interaction.client.guilds.fetch(guildId);
+
+        if (!guild.members.me.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+            const embed = new EmbedBuilder().setColor(Colors.Red).setDescription("I don't have permissions to get the invites of that guild.");
+            return interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
+        const invites = await guild.invites.fetch();
+        const embed = new EmbedBuilder().setColor(Colors.Blue).setFooter({ text: guild.name, iconURL: guild.iconURL() }).setTimestamp();
+
+        if (invites.size > 0) {
+            embed.setTitle(`Invites of the guild ${guild.name}`);
+            embed.setDescription(invites.map((invite) => `${invitation} ${invite.url} by **${invite.inviter.username}** (${invite.uses} uses)`).join("\n"));
+        } else {
+            embed.setDescription("There is no invites to get.");
+        }
+
+        return interaction.reply({ embeds: [embed] });
+    },
+};
